7dd6785b0e7c713d4966b8253829d45fa1d7271a
Martin Ahindura <martinahindura@gmail.com>
2019-09-01 09:01:43 +0300

    Add navigation on the home screen

--- screens/AccountScreen.tsx
+++ screens/AccountScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, processColor } from "react-native";
 
 export default function AccountScreen() {
   return (
--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -2,66 +2,75 @@ import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Theme from "../config/theme";
+import getApiConnector from "../services/get-api-connector";
+import { apiUrl, apiConfig } from "../config/api";
+
+const apiConnector = getApiConnector(apiUrl, apiConfig);
 
 export default function HomeScreen(props) {
   const goToLogin = () => {
-    props.navigation.navigate("Login");
+    props.navigation.navigate("Login", { apiConnector });
   };
 
-  return (
-    <View style={styles.container}>
-      <View style={[styles.container, { marginTop: "20%" }]}>
-        <Text
-          style={[
-            styles.text,
-            {
-              fontSize: 24,
-              fontWeight: "600",
-              color: Theme.primary
-            }
-          ]}
-        >
-          UnitedCHURCH
-        </Text>
-      </View>
-      <View style={{ height: 130, width: 130, flex: 2 }}>
-        <Image
-          style={styles.image}
-          source={require("../assets/images/homeCross.png")}
-        />
-      </View>
+  try {
+    apiConnector.client.reAuthenticate();
+    props.navigation.navigate("NewsFeed", { apiConnector });
+  } catch (error) {
+    return (
       <View style={styles.container}>
-        <Text
-          style={[
-            styles.text,
-            {
-              fontSize: 18
-            }
-          ]}
-        >
-          Sharing Across a United Church
-        </Text>
-      </View>
-      <TouchableOpacity
-        style={{
-          backgroundColor: Theme.backGround,
-          marginBottom: "20%"
-        }}
-        onPress={goToLogin}
-      >
-        <LinearGradient
-          colors={[Theme.primary, Theme.fadedPrimary]}
-          start={[0, 0.5]}
-          end={[1, 0.5]}
-          style={styles.callToActionBtn}
-        >
-          <Text style={[styles.text, styles.callToActionText]}>
-            Get Started
+        <View style={[styles.container, { marginTop: "20%" }]}>
+          <Text
+            style={[
+              styles.text,
+              {
+                fontSize: 24,
+                fontWeight: "600",
+                color: Theme.primary
+              }
+            ]}
+          >
+            UnitedCHURCH
           </Text>
-        </LinearGradient>
-      </TouchableOpacity>
-    </View>
-  );
+        </View>
+        <View style={{ height: 130, width: 130, flex: 2 }}>
+          <Image
+            style={styles.image}
+            source={require("../assets/images/homeCross.png")}
+          />
+        </View>
+        <View style={styles.container}>
+          <Text
+            style={[
+              styles.text,
+              {
+                fontSize: 18
+              }
+            ]}
+          >
+            Sharing Across a United Church
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: Theme.backGround,
+            marginBottom: "20%"
+          }}
+          onPress={goToLogin}
+        >
+          <LinearGradient
+            colors={[Theme.primary, Theme.fadedPrimary]}
+            start={[0, 0.5]}
+            end={[1, 0.5]}
+            style={styles.callToActionBtn}
+          >
+            <Text style={[styles.text, styles.callToActionText]}>
+              Get Started
+            </Text>
+          </LinearGradient>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
